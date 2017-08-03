b603592573c26629b0049ebdfab7d5462616b7b0
David <dmogollon@uninorte.edu.co>
2017-08-03 16:58:31 +0000

    added db credentials for heroku

--- config/database.js
+++ config/database.js
@@ -2,13 +2,15 @@
 module.exports = {
     'connection': {
     	'connectionLimit': 50,
-        'host': 'localhost',
-        'user': 'root',
-        'password': ''
+        'host': 'us-cdbr-iron-east-05.cleardb.net',
+        'user': 'b262df3ec91ef4',
+        'password': '22f2c93e'
        
     },
-	'database': 'ticketmanagerx',
+	'database': 'heroku_6a09f24a7d57904',
     'users_table': 'users',
     'message_table':'messages',
     'ticket_table':'tickets',
 };
+
+// mysql://b262df3ec91ef4:22f2c93e@us-cdbr-iron-east-05.cleardb.net/heroku_6a09f24a7d57904?reconnect=true
